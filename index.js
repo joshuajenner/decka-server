ef18cc12d6c465128db2dc6cf28042bc323b6f61
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2021-03-06 18:45:16 +0800

    yep

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ const port = process.env.PORT || 3000;
 
 server.use(bodyParser.json());
 server.use(cors({
-    origin: 'http://localhost:5000'
-  })); 
+  origin: 'http://localhost:5000'
+}));
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-        clientEmail: process.env.CLIENT_EMAIL,
-        projectId: process.env.PROJECT_ID
-    }),
-    databaseURL: 'https://' + process.env.PROJECT_ID + '.firebaseio.com'
+  credential: admin.credential.cert({
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.CLIENT_EMAIL,
+    projectId: process.env.PROJECT_ID
+  }),
+  databaseURL: 'https://' + process.env.PROJECT_ID + '.firebaseio.com'
 });
 
 const db = admin.firestore();
@@ -29,40 +29,44 @@ const db = admin.firestore();
 
 
 server.get('/', (req, res) => {
-    res.send('Decka Server');
+  res.send('Decka Server');
 })
 
 server.post('/login', async (req, res) => {
-    admin
-  .auth()
-  .getUserByEmail(req.body.email)
-  .then((userRecord) => {
-    // See the UserRecord reference doc for the contents of userRecord.
-    console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-    res.send(userRecord.toJSON());
-  })
-  .catch((error) => {
-    console.log('Error fetching user data:', error);
-  });
+  admin
+    .auth()
+    .getUserByEmail(req.body.email)
+    .then((userRecord) => {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+      res.send(userRecord.toJSON());
+    })
+    .catch((error) => {
+      console.log('Error fetching user data:', error);
+    });
 })
 
 server.post('/newdeck', async (req, res) => {
-    const doc = await db.collection(req.body.uid).add({
-        title: "New Deck",
-        content: "This is a fresh deck."
-    });
+  const doc = await db.collection(req.body.uid).add({
+    title: "New Deck",
+    content: "This is a fresh deck."
+  });
 
-    console.log(doc.id);
+  console.log(doc.id);
 });
 server.post('/getdecks', async (req, res) => {
-    const snapshot = await db.collection(req.body.uid).get();
-    let decks = [];
-    snapshot.forEach(doc => {
+  const snapshot = await db.collection(req.body.uid).get();
+  let decks = [];
+  snapshot.forEach(doc => {
     //   console.log(doc.id, '=>', doc.data());
-    decks.push(doc.data())
-    });
-    res.send(decks)
+    decks.push({
+      id: doc.id,
+      data: doc.data()
+    }
+    )
+  });
+  res.send(decks)
 });
 server.listen(port, hostname, () => {
-    console.log(`Server is listening at https://127.0.0.1:300`);
+  console.log(`Server is listening at https://127.0.0.1:300`);
 });
