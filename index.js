207dc866a8cdf922c3feea1cd147aa6480a9e054
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2021-03-11 18:44:48 +0800

    more update and deletes

--- index.js
+++ index.js
@@ -32,27 +32,44 @@ server.get('/', (req, res) => {
   res.send('Decka Server');
 })
 
-server.post('/login', async (req, res) => {
-  admin
-    .auth()
-    .getUserByEmail(req.body.email)
-    .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-      res.send(userRecord.toJSON());
-    })
-    .catch((error) => {
-      console.log('Error fetching user data:', error);
-    });
-})
+
+// ---------------------------------------------- Authentication
+
+// server.post('/login', async (req, res) => {
+//   admin
+//     .auth()
+//     .getUserByEmail(req.body.email)
+//     .then((userRecord) => {
+//       // See the UserRecord reference doc for the contents of userRecord.
+//       console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+//       res.send(userRecord.toJSON());
+//     })
+//     .catch((error) => {
+//       console.log('Error fetching user data:', error);
+//     });
+// })
+
+// ---------------------------------------------- New
 
 server.post('/newdeck', async (req, res) => {
   const doc = await db.collection(req.body.uid).add({
-    title: "New Deck",
-    content: "This is a fresh deck."
-  });
-  console.log(doc.id);
+    title: req.body.title
+  }).then(
+    res.send("Success")
+  );
 });
+
+server.post('/newcard', async (req, res) => {
+  const data = {
+    title: req.body.title,
+    content: req.body.content
+  }
+  const createCard = await db.collection(req.body.uid).doc(req.body.did).collection("cards").add(data).then(
+    res.send("Success")
+  );
+});
+
+// ---------------------------------------------- Get
 
 server.post('/getdecks', async (req, res) => {
   const snapshot = await db.collection(req.body.uid).get();
@@ -82,13 +99,25 @@ server.post('/getdeckdata', async (req, res) => {
   res.send(cards)
 });
 
-server.post('/newcard', async (req, res) => {
-  const data = {
-    title: req.body.title,
-    content: req.body.content
-  }
-  const createCard = await db.collection(req.body.uid).doc(req.body.did).collection("cards").add(data);
+// ---------------------------------------------- Update
+
+server.post('/updatedeck', async (req, res) => {
+  await db.collection(req.body.uid).doc(req.body.did).update({
+    title: req.body.title
+  }).then(
+    res.send("Success")
+  );
 });
+
+// ---------------------------------------------- Delete
+
+server.post('/deletedeck', async (req, res) => {
+  await db.collection(req.body.uid).doc(req.body.did).delete().then(
+    res.send("Success")
+  );
+});
+
+// ---------------------------------------------- 
 
 server.listen(port, hostname, () => {
   console.log(`Server is listening at https://127.0.0.1:300`);
